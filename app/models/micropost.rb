1c4bcec75205dcb5992fa0afceed89cacb63bba0
jbnewlin <jnewlin@purdue.edu>
2015-11-02 01:50:26 +0000

    Fixed everything, it's obviously perfect

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-  validates :content, length: { maximum: 140, minimum: 1 }
+  validates :content, length: { maximum: 140 },
+  presence: true
 end
